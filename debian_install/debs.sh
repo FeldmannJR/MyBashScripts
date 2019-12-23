def35b680be4b113384316f56699272ace8e2d00
Carlos Feldmann <feldmannjunior@gmail.com>
2019-12-23 00:57:30 -0300

    Installing MultiMC(MINECRAFT)

--- debian_install/debs.sh
+++ debian_install/debs.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 set -x
-export TEMP_DIR=./temp  
+export SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+export TEMP_DIR=$SCRIPT_DIR/temp  
 DEBS=$TEMP_DIR/debs
+# Delete old versions
 rm -rf $DEBS
 mkdir -p $DEBS
 cd $DEBS
+
+#=================================================
 # Steam
 wget https://steamcdn-a.akamaihd.net/client/installer/steam.deb
 # Slack
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.2.0-amd64.deb
 # Discord
 wget https://dl.discordapp.net/apps/linux/0.0.9/discord-0.0.9.deb
+# Multimc
+wget https://files.multimc.org/downloads/multimc_1.3-1.deb
+#=================================================
+
 # Installing
 dpkg -i *.deb
 # Cleaning Installation Files
 cd $TEMP_DIR
-rm -rf $DEBS
 # Fix dependencies
 apt-get --fix-broken install -y
